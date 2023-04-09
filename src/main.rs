2b38d7bf1d30295fffea3ab8ccec0ab3cb054d6b
ittipol <s6303051623101@email.kmutnb.ac.th>
2023-04-09 23:43:31 +0700

    hello-world:0.0.1

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() -> std::io::Result<()> {
             .configure(cart_routes::config)
             .configure(shipping_routes::config)
    })
-   .bind("127.0.0.1:8080")?
+   .bind("0.0.0.0:8080")?
    .run()
    .await
 }
